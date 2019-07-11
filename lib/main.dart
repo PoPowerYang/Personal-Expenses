c4e0beeb933b5ba0654c07749aa5e0134c2eef1a
PoPowerYang <yangsunsunshine@gmail.com>
2019-07-11 12:54:13 -0400

    first commit, delete button added

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+import './widgets/chart.dart';
+import './widgets/new_transaction.dart';
+import './widgets/trascation_list.dart';
+import './models/trascation.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Personal Expenses',
+
+      theme: ThemeData(
+        primarySwatch: Colors.red, //automatically fix color for different widgets
+        accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          title: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            )
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            title: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              )
+            ),
+          )
+        ),
+
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+    final List<Transaction> _userTransactions = [
+  
+    ];
+
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7)
+          ),
+        );
+    }).toList();
+  }
+
+  void _addNewTransaction(String txTitle, double txAmount, DateTime choesnDate) {
+    final newTx = Transaction(
+        title: txTitle, 
+        amount: txAmount,
+        date: choesnDate,
+        id: DateTime.now().toString(),
+      );
+
+      setState(() {
+        _userTransactions.add(newTx); 
+      });
+  }
+
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransactions.retainWhere((tx) => tx.id == id);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx, 
+      builder: (_) {
+        return NewTransaction(_addNewTransaction);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Personal Expenses',
+              style: TextStyle(
+                fontFamily: 'Open Sans',
+              ),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.view_headline),
+                onPressed:() => _startAddNewTransaction(context),
+              ),
+            ],
+          ),
+            body: SingleChildScrollView(
+              child: 
+                Column(
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Chart(_recentTransactions),
+                  TransactionList(_userTransactions, _deleteTransaction),
+                ],
+              ),
+            ),
+
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, 
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.assignment),
+              backgroundColor: Colors.amber,
+              onPressed: () => _startAddNewTransaction(context),
+            ),
+          );
+  }
+}
---
+++ lib/widgets/trascation_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/trascation.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> _listOfTx;
+  final Function deleteTx;
+  TransactionList(this._listOfTx, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+        child: 
+        _listOfTx.isEmpty ? Column(children: <Widget>[
+          Text(
+            'No transaction added yet!',
+            style: Theme.of(context).textTheme.title,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 200,
+            child: Image.asset(
+              'assets/image/waiting.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],) :ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+                elevation: 10,
+                child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: FittedBox(
+                        child: Text('\$${_listOfTx[index].amount}')
+                      ),
+                  ),
+                ),
+                title: Text(_listOfTx[index].title, style: Theme.of(context).textTheme.title,),
+                subtitle: Text(DateFormat.yMMMd().format(_listOfTx[index].date)),
+                trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => deleteTx(_listOfTx[index].id),
+                  ),
+              ),
+            );
+          },
+          itemCount: _listOfTx.length,
+          ),
+    );
+  }
+}
