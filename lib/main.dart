b54ac6afe2853cedb7799d25177ef98b086bf370
PoPowerYang <yangsunsunshine@gmail.com>
2019-07-11 14:56:00 -0400

    UI update

--- lib/main.dart
+++ lib/main.dart
@@ -115,13 +115,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
                     Container(
-                      height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.4,
+                      height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.3,
                       child: Chart(_recentTransactions)
                     ),
                   // Expanded(
                     // child: 
                     Container(
-                      height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.6,
+                      height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
                       child: TransactionList(_userTransactions, _deleteTransaction)
                     ),
                   // ),
