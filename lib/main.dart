f5b9b2c3e2567cededc3432bc1f7600bdfa7a37e
PoPowerYang <yangsunsunshine@gmail.com>
2019-07-11 20:39:51 -0400

    more ios support added, but due to the limit that the cupertino material app, I did not use cupertino app to futher the detail for the IOS platform

--- lib/main.dart
+++ lib/main.dart
@@ -144,7 +144,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   if(isLandScape) Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Text('Show Chart'),
+                      Text(
+                          'Show Chart', 
+                          style: Theme.of(context).textTheme.title,
+                      ),
                       Switch.adaptive(
                         activeColor: Theme.of(context).accentColor,
                         value: _showChart,
