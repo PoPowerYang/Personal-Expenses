ca8476d578be951d9cb58a36e5d472ab23af2263
PoPowerYang <yangsunsunshine@gmail.com>
2019-07-11 21:33:06 -0400

    a few changes :)

--- lib/main.dart
+++ lib/main.dart
@@ -51,21 +51,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    final List<Transaction> _userTransactions = [
-  
-    ];
+  final List<Transaction> _userTransactions = [
 
-    bool _showChart = false;
-
-  List<Transaction> get _recentTransactions {
-    return _userTransactions.where((tx) {
-      return tx.date.isAfter(
-        DateTime.now().subtract(
-          Duration(days: 7)
-          ),
-        );
-    }).toList();
-  }
+  ];
 
   void _addNewTransaction(String txTitle, double txAmount, DateTime choesnDate) {
     final newTx = Transaction(
@@ -136,53 +124,6 @@ class _MyHomePageState extends State<MyHomePage> {
                               - mediaQuery.padding.top) * 1,
                       child: TransactionList(_userTransactions, _deleteTransaction)
                     );
-    // final pageBody = SafeArea(child: SingleChildScrollView(
-    //           child: 
-    //             Column(
-    //             // mainAxisAlignment: MainAxisAlignment.spaceAround,
-    //             crossAxisAlignment: CrossAxisAlignment.stretch,
-    //             children: <Widget>[
-    //               if(isLandScape) Row(
-    //                 mainAxisAlignment: MainAxisAlignment.center,
-    //                 children: <Widget>[
-    //                   Text(
-    //                       'Show Chart', 
-    //                       style: Theme.of(context).textTheme.title,
-    //                   ),
-    //                   Switch.adaptive(
-    //                     activeColor: Theme.of(context).accentColor,
-    //                     value: _showChart,
-    //                     onChanged: (val) {
-    //                       setState(() {
-    //                         _showChart = val;
-    //                       });
-    //                     },
-    //                   ),
-    //                 ],
-    //               ),
-    //               if(!isLandScape) Container(
-    //                 height: (mediaQuery.size.height - appBar.preferredSize.height
-    //                          - mediaQuery.padding.top) * 0.3,
-    //                 child: Chart(_recentTransactions)
-    //               ),
-
-    //               if(!isLandScape) txListWidge,
-
-    //               if(isLandScape)
-    //                 _showChart 
-    //                 ? Container(
-    //                   height: (mediaQuery.size.height 
-    //                           - appBar.preferredSize.height 
-    //                           - mediaQuery.padding.top) * 0.7,
-    //                   child: Chart(_recentTransactions)
-    //                 )
-    //               // Expanded(
-    //                 // child: 
-    //                 : txListWidge
-    //               // ),
-    //             ],
-    //           ),
-    //         ),);
 
     return Platform.isIOS 
         ? CupertinoPageScaffold(
